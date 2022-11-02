6177450a600ffcf2bd32ebfce07c65c4fb51a4dc
Ha minu <114812091+lumen4723@users.noreply.github.com>
2022-11-02 16:04:57 +0900

    처음 테스트용 파일

---
+++ testnote.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout << "HelloWorld!1";
+    cout << "HelloWorld!2";
+    cout << "HelloWorld!3";
+    cout << "HelloWorld!4";
+    cout << "HelloWorld!5";
+    cout << "HelloWorld!6";
+    return 0;
+}
