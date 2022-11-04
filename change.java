1ca759d14179e73796b03c989f4d8e3610569a1a
haminu <you@example.com>
2022-11-04 23:08:36 +0900

    상훈, 상배 과제 할당

---
+++ change.java
@@ -0,0 +1,8 @@
+public change {
+    public static void main(String[] args) {
+        int[] a = {6, 5, 3, 9, 8, 7, 1, 2, 4};
+        // 상훈이가 버블소트함수를 제작하고 정렬전 a와 정렬후 b를 출력하시오.
+
+        // 상배가 이진탐색 함수를 제작하고 b에서 3의 인덱스를 찾으시오.
+    }
+}
