69de4f6a84c7be6c46a96b579444fe0d38d6ffe3
Berryl1 <lsb001010@gmail.com>
2022-11-04 21:57:49 +0900

    first

--- testnote.cpp
+++ testnote.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main() {
-    cout << "HelloWorld!3";
+    cout << "HelloWorld!4";
     cout << "HelloWorld!2";
     cout << "HelloWorld!3";
     cout << "HelloWorld!4";
     cout << "HelloWorld!8";
-    cout << "HelloWorld!7";
+    cout << "HelloWorld!9";
     return 0;
 }
