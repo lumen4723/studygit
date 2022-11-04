992cf212504b2c98c4188917232263f6c8a4749d
jeonsanghoon711 <wjstkdgns711@naver.com>
2022-11-04 23:39:46 +0900

    전상훈수정

--- change.java
+++ change.java
@@ -1,7 +1,22 @@
+import java.util.Arrays;
+
 public class change {
     public static void main(String[] args) {
         int[] a = {6, 5, 3, 9, 8, 7, 1, 2, 4};
         // 상훈이가 버블소트함수를 제작하고 정렬전 a와 정렬후 b를 출력하시오.
+        System.out.println("정렬 전 a : " + Arrays.toString(a));
+        for(int i = 0; i < a.length-1; i++){
+            int temp = 0;
+            for(int j = 0; j < a.length-1; j++){
+                if(a[j] > a[j+1]){
+                    temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                }
+
+            }
+        }
+        System.out.println("정렬 후 b : " + Arrays.toString(a));
 
         // 상배가 이진탐색 함수를 제작하고 b에서 3의 인덱스를 찾으시오.
     }
